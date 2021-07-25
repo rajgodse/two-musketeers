9a5f918fb9968f9fe5b823f1328cbec1a02b0b6d
Rajeev Godse <rajgodse@gmail.com>
2021-07-24 23:13:08 -0700

    Changed the way workers keep track of resource queries given that bases can't write rock art.

--- src/naiveversion2/Worker.java
+++ src/naiveversion2/Worker.java
@@ -2,23 +2,33 @@ package naiveversion2;
 
 import aic2021.user.*;
 import naiveversion2.MyUnit;
+import naiveversion2.common.Comms;
+import naiveversion2.common.UnitTarget;
 import sun.security.krb5.internal.crypto.Des;
 
 public class Worker extends MyUnit {
 
+    // TO DO: change to make sure that the square is free
+    final Location resourceQueryCountLocation = new Location (home.x + 1, home.y + 1);
+
     static class WorkerStates {
-        public static int numValues() { return 3; }
         public static int GATHERING() { return 0; }
         public static int LIGHTINGTHEWAY() { return 1; }
         public static int NAVIGATING() { return 2; }
+        public static int GETTINGTASK() { return 3; }
+        public static int numValues() { return 4; }
     }
     Location prevDestination;
     Worker(UnitController uc) {
         super(uc);
+        currentState = WorkerStates.NAVIGATING();
+        Destination = resourceQueryCountLocation;
     }
 
     boolean torchLighted = false;
     boolean smoke = false;
+    boolean hasSeenResourceQueries = false;
+    int resourceQueriesSeen = 0;
 
     void lightTheWay(UnitInfo myInfo){
         return;
@@ -34,6 +44,14 @@ public class Worker extends MyUnit {
             uc.gatherResources();
             uc.println("gathering resources");
         }
+        else {
+            uc.println("All materials gathered at " + Destination.toString());
+            uc.println("Looking home and looking for new tasks");
+            prevDestination = resourceQueryCountLocation;
+            Destination = home;
+            currentState = WorkerStates.NAVIGATING();
+            return;
+        }
         Boolean food = uc.senseResources(0, Resource.FOOD).length == 0;
         Boolean wood = uc.senseResources(0, Resource.WOOD).length == 0;
         Boolean stone = uc.senseResources(0,Resource.STONE).length == 0;
@@ -47,11 +65,52 @@ public class Worker extends MyUnit {
         }
     }
 
+    void handleSignals(){
+        for(int sig: Signals) {
+            if(comms.getSmokeSignal(sig) == Comms.SmokeSignal.LOCATION()) {
+                int locationType = comms.getLocationType(sig);
+                uc.println("receiving a signal, location type: " + locationType);
+                if(comms.locationTypeIsResource(locationType)) {
+                    uc.println("adding to the resource queue");
+                    resourceQueue.add(new ResourceInfo(comms.locationTypeToResource(locationType), 0, comms.getLocation(sig)));
+                }
+            }
+        }
+    }
+
+    void readResourceQueryCount(Location currLoc) {
+        if (uc.canRead(currLoc)) {
+            currentState = WorkerStates.GETTINGTASK();
+            int queryCount = uc.read(currLoc);
+            while (resourceQueriesSeen < queryCount) {
+                resourceQueue.poll();
+                resourceQueriesSeen++;
+            }
+            ResourceInfo info = resourceQueue.poll();
+            if (info == null) {
+                return;
+            }
+            if (uc.canDraw(queryCount + 1)) {
+                uc.draw(queryCount + 1);
+            }
+            else {
+                uc.println("Can't write resource query count");
+            }
+            currentState = WorkerStates.NAVIGATING();
+            Destination = info.location;
+        }
+        else {
+            uc.println("Can't read resource query count");
+        }
+    }
+
     void playRound(){
-        currentState = WorkerStates.LIGHTINGTHEWAY();
+        // currentState = WorkerStates.LIGHTINGTHEWAY();
         UnitInfo myInfo = uc.getInfo();
         Boolean torchLighted = keepItLight();
         Location currLoc = uc.getLocation();
+        handleSignals();
+        uc.println("My current state is " + currentState);
         if(Destination != null) {
             uc.println("Destination set, Navigation set to true");
             currentState = WorkerStates.NAVIGATING();
@@ -60,9 +119,11 @@ public class Worker extends MyUnit {
             // testing navigation
             if(torchLighted || uc.senseIllumination(uc.getLocation()) == 16) {
                 if(!uc.getLocation().equals(Destination)) {
+                    uc.println("Navigating to " + Destination.x + " " + Destination.y);
+                    uc.println("Moving in this direction: " + nav.goToLocation(Destination));
                     move(nav.goToLocation(Destination));
                 }
-                else if (uc.getLocation().equals(Destination) && Destination.distanceSquared(home) == 1) {
+                else if (Destination.distanceSquared(home) == 1) { // removed extra check for uc.getLocation().equals(Destination)
                     if(uc.canDeposit()) {
                         uc.println("depositing Resources, going to get more");
                         uc.deposit();
@@ -70,6 +131,20 @@ public class Worker extends MyUnit {
                     }
                     else {
                         uc.println("cannot deposit Resources");
+                    }
+                }
+                else if (Destination.distanceSquared(resourceQueryCountLocation) == 1) {
+                    if (!hasSeenResourceQueries) {
+                        hasSeenResourceQueries = true;
+                        if (uc.canRead(currLoc)) {
+                            resourceQueriesSeen = uc.read(currLoc);
+                        }
+                        else {
+                            uc.println("Can't read resource query count after INIT");
+                        }
+                    }
+                    else {
+                        readResourceQueryCount(currLoc);
                     }
                 }
                 else {
@@ -80,6 +155,8 @@ public class Worker extends MyUnit {
             lightTheWay(myInfo);
         } if(currentState == WorkerStates.GATHERING()) {
             gather(myInfo);
+        } if (currentState == WorkerStates.GETTINGTASK()) {
+            readResourceQueryCount(currLoc);
         }
         if(uc.hasResearched(Technology.MILITARY_TRAINING, uc.getTeam())) {
             if(uc.canSpawn(UnitType.BARRACKS,Direction.NORTH)) {
